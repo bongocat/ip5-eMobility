316751882fb5befd438434d3d88a17c580530634
René Achterberg <rene.achterberg@bluewin.ch>
2020-11-24 10:20:13 +0100

    add vuetify

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,89 +1,92 @@
 <template>
-   <div class="container-fluid">
-      <div class = "heading">
-         <h1>Anstehende Rechnungen</h1>
-         <h5>(Nächste 30 Tage)</h5>
-      </div>
-         <table class="table table-dark">
-            <thead>
-            <tr>
-               <th @click='sort(item)' v-for="(item, index) in columnNames" v-bind:key="index" scope="col">{{item}}</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(item, index1) in upcomingInvoices"  v-bind:key="index1">
-               <td v-for="(category, index2) in item"  v-bind:key="index2">
-                  {{category}}
-               </td>
-            </tr>
-            </tbody>
-         </table>
-      debug: sort={{currentSort}}, dir={{currentSortDir}}
-      <p>
-         <button @click="prevPage">Previous</button>
-         <button @click="nextPage">Next</button>
-      </p>
+   <div>
+      <h1 id = invoices>Anstehende Rechnungen</h1>
+      <v-data-table :headers="columnNames" :items="upcomingInvoices" :items-per-page="10" class="elevation-1"></v-data-table>
    </div>
 </template>
 
 <script>
-    export default {
-        name: "Dashboard",
-       data(){
-           return {
-              upcomingInvoices: [
-                 {Liegenschaft: "L1", Betrag: 125, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
-                 {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
-                 {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"}
-              ],
-              columnNames: ["Liegenschaft", "Betrag", "Rechnungsart", "Mieter", "Immobilienverwaltung", "Fällig Am"],
-              currentSort:'name',
-              currentSortDir:'asc',
-              pageSize:3,
-              currentPage:1
-           };
-       },
-       methods:{
+   export default {
+      name: "Dashboard",
+      data(){
+         return {
+            upcomingInvoices: [
+               {Liegenschaft: "L1", Betrag: 125, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L1", Betrag: 125, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L1", Betrag: 125, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L1", Betrag: 125, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L1", Betrag: 125, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L1", Betrag: 125, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L1", Betrag: 125, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L2", Betrag: 144, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"},
+               {Liegenschaft: "L3", Betrag: 172, Rechnungsart: "Serviceabo", Mieter: "Mietermann", Immobilienverwaltung: "Verwaltung 1", fälligAm: "31.12.2020"}
+            ],
+            columnNames: [
+               {text: 'Liegenschaft', value: 'Liegenschaft'},
+               {text: 'Betrag', value: 'Betrag'},
+               {text: 'Rechnungsart', value: 'Rechnungsart'},
+               {text: 'Mieter', value: 'Mieter'},
+               {text: 'Immobilienverwaltung', value: 'Immobilienverwaltung'},
+               {text: 'Fällig Am', value: 'fälligAm'},
+            ],
+            currentSort:'name',
+            currentSortDir:'asc',
+            pageSize:7,
+            currentPage:1
+         };
+      },
+      methods:{
 
-          sort(s) {
-             //if s == current sort, reverse
-             if(s === this.currentSort) {
-                this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
-             }
-             this.currentSort = s;
-             this.upcomingInvoices.sort((a,b) => {
-                   let modifier = 1;
-                   if(this.currentSortDir === 'desc') modifier = -1;
-                   if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
-                   if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
-                   return 0;
-                }).filter((row, index) => {
-                   let start = (this.currentPage-1)*this.pageSize;
-                   let end = this.currentPage*this.pageSize;
-                   if(index >= start && index < end) return true;
-                });
-             },
-          nextPage:function() {
-             if((this.currentPage*this.pageSize) < this.cats.length) this.currentPage++;
-          },
-          prevPage:function() {
-             if(this.currentPage > 1) this.currentPage--;
-          },
-       },
-       watch: {
-       }
-    }
+         sort(s) {
+            //if s == current sort, reverse
+            if(s === this.currentSort) {
+               this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+            }
+            this.currentSort = s;
+            this.upcomingInvoices.sort((a,b) => {
+               let modifier = 1;
+               if(this.currentSortDir === 'desc') modifier = -1;
+               if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+               if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+               return 0;
+            }).filter((row, index) => {
+               let start = (this.currentPage-1)*this.pageSize;
+               let end = this.currentPage*this.pageSize;
+               if(index >= start && index < end) return true;
+            });
+         },
+         nextPage:function() {
+            if((this.currentPage*this.pageSize) < this.upcomingInvoices.length) this.currentPage++;
+         },
+         prevPage:function() {
+            if(this.currentPage > 1) this.currentPage--;
+         },
+      },
+      watch: {
+      }
+   }
 
 </script>
 
 <style scoped>
    h1, h5 {
       margin: 20px;
-      float: left !important;
       vertical-align: center;
+      clear:both !important;
    }
 
-   .heading {
-      vertical-align: center;
-   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from "vue-router";
-import MegalogDashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard";
 import Invoices from "./components/Invoices";
 import Loads from "./components/Loads";
 import Facilities from "./components/Facilities";
 import Users from "./components/Users";
+import vuetify from './plugins/vuetify';
 
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 const routes = [
-  {path: '/dashboard', component: MegalogDashboard, name: 'Dashboard'},
+  {path: '/dashboard', component: Dashboard, name: 'Dashboard'},
   {path: '/invoices', component: Invoices, name: "Rechnungen"},
   {path: '/loads', component: Loads, name: 'Loads'},
   {path: '/facilities', component: Facilities, name: "Anlagen"},
@@ -26,5 +27,6 @@ const router = new VueRouter({
 
 new Vue({
   render: h => h(App),
-  router,
+  vuetify,
+  router
 }).$mount('#app')
