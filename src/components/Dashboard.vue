515d428749b9c8b8e9fa6e1c3854f71d84c67264
René Achterberg <rene.achterberg@bluewin.ch>
2020-11-26 11:37:23 +0100

    add filter fields

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,26 +1,164 @@
 <template>
    <v-main>
       <v-container fluid>
-         <v-card style="margin-top: 20px">
+         <v-card style="margin-top: 20px" :elevation="5">
             <v-card-title>
-               <h1>Kürzlich bezahlte Rechnungen</h1>
+               <h1>Anstehende Rechnungen</h1>
+               <v-badge :content="upcomingInvoices.length" :value="upcomingInvoices.length" color="success" inline/>
             </v-card-title>
-            <v-data-table
-                    dense
-                    :headers="columnNames"
-                    :items="upcomingInvoices"
-                    class="elevation-1"
-                    :items-per-page="5">
-               <template v-slot:item.actions="{item}">
-                  <GenerateInvoice :invoice="item"></GenerateInvoice>
-               </template>
-            </v-data-table>
+            <v-card-subtitle style="padding-left: 20px; padding-top: -30px">(nächste 30 Tage)</v-card-subtitle>
+            <v-card-text>
+               <v-row>
+                  <v-col>
+                     <v-overflow-btn
+                             deletable-chips
+                             dense
+                             editable
+                             full-width="false"
+                             :items="getUniqueProperties"
+                             label="Anlagen"
+                             item-value="text"
+                     ></v-overflow-btn>
+                  </v-col>
+                  <v-col>
+                     <v-overflow-btn
+                             deletable-chips
+                             dense
+                             editable
+                             full-width="false"
+                             :items="getUniqueAdministration"
+                             label="Verwaltungen"
+                             item-value="text"
+                     ></v-overflow-btn>
+                  </v-col>
+                  <v-col>
+                     <v-overflow-btn
+                             deletable-chips
+                             dense
+                             editable
+                             full-width="false"
+                             :items="getUniqueTenants"
+                             label="Mieter"
+                             item-value="text"
+                     ></v-overflow-btn>
+                  </v-col>
+                  <v-col>
+                     <v-overflow-btn
+                             deletable-chips
+                             dense
+                             editable
+                             full-width="false"
+                             :items="getUniqueInvoiceCategory"
+                             label="Rechnungsart"
+                             item-value="text"
+                     ></v-overflow-btn>
+                  </v-col>
+                  <v-col>
+                     <v-menu
+                             ref="menuStartingDate"
+                             v-model="menuStartingDate"
+                             :close-on-content-click="false"
+                             :return-value.sync="startingDate"
+                             transition="scale-transition"
+                             offset-y
+                             min-width="290px"
+                     >
+                        <template v-slot:activator="{ on, attrs }">
+                           <v-text-field
+                                   v-model="startingDate"
+                                   label="Startdatum"
+                                   prepend-icon="mdi-calendar"
+                                   readonly
+                                   v-bind="attrs"
+                                   v-on="on"
+                           ></v-text-field>
+                        </template>
+                        <v-date-picker
+                                v-model="startingDate"
+                                no-title
+                                scrollable
+                        >
+                           <v-spacer></v-spacer>
+                           <v-btn
+                                   text
+                                   color="primary"
+                                   @click="menuStartingDate = false"
+                           >
+                              Cancel
+                           </v-btn>
+                           <v-btn
+                                   text
+                                   color="primary"
+                                   @click="$refs.menuStartingDate.save(startingDate)"
+                           >
+                              OK
+                           </v-btn>
+                        </v-date-picker>
+                     </v-menu>
+                  </v-col>
+               <v-col>
+                  <v-menu
+                          ref="menuEndDate"
+                          v-model="menuEndDate"
+                          :close-on-content-click="false"
+                          :return-value.sync="endDate"
+                          transition="scale-transition"
+                          offset-y
+                          min-width="290px"
+                  >
+                     <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                                v-model="endDate"
+                                label="Enddatum"
+                                prepend-icon="mdi-calendar"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                        ></v-text-field>
+                     </template>
+                     <v-date-picker
+                             v-model="endDate"
+                             no-title
+                             scrollable
+                     >
+                        <v-spacer></v-spacer>
+                        <v-btn
+                                text
+                                color="primary"
+                                @click="menuEndDate = false"
+                        >
+                           Cancel
+                        </v-btn>
+                        <v-btn
+                                text
+                                color="primary"
+                                @click="$refs.menuEndDate.save(endDate)"
+                        >
+                           OK
+                        </v-btn>
+                     </v-date-picker>
+                  </v-menu>
+               </v-col>
+               </v-row>
+               <v-data-table
+                       dense
+                       :headers="columnNames"
+                       :items="upcomingInvoices"
+                       class="elevation-1"
+                       :items-per-page="5">
+                  <template v-slot:item.actions="{item}">
+                     <GenerateInvoice :invoice="item"></GenerateInvoice>
+                  </template>
+               </v-data-table>
+            </v-card-text>
          </v-card>
-      <v-card style="margin-top: 20px">
+      <v-card style="margin-top: 20px" :elevation="5">
          <v-card-title>
             <h1>Offene Rechnungen</h1>
+            <v-badge :content="upcomingInvoices.length" :value="upcomingInvoices.length" color="success" inline/>
          </v-card-title>
-         <v-data-table
+         <v-card-text>
+            <v-data-table
                  v-model="selected"
                  item-key="ID"
                  show-select
@@ -30,38 +168,42 @@
                  :items="upcomingInvoices"
                  class="elevation-1"
                  :items-per-page="5">
-               <template v-slot:item.actions="{item}">
-                  <v-btn color="success" x-small class="mr-2" @click="markAsPaid([item])">
-                     Als Bezahlt markieren
-                  </v-btn>
-               </template>
+            <template v-slot:item.actions="{item}">
+               <v-btn color="success" x-small class="mr-2" @click="markAsPaid([item])">
+                  Als Bezahlt markieren
+               </v-btn>
+            </template>
          </v-data-table>
+         </v-card-text>
          <v-card-actions>
             <v-btn small color="blue" @click="markAsPaid(selected), resetSelected()">
                Ausgewählte als bezahlt markieren
             </v-btn>
          </v-card-actions>
          </v-card>
-      <v-card style="margin-top: 20px">
+      <v-card style="margin-top: 20px" :elevation="5">
          <v-card-title>
             <h1>Kürzlich bezahlte Rechnungen</h1>
+            <v-badge :content="paidInvoices.length" :value="paidInvoices.length" color="success" inline/>
          </v-card-title>
-         <v-data-table
-                 v-model="selected"
-                 item-key="ID"
-                 dense
-                 :headers="columnNames"
-                 :items="paidInvoices"
-                 class="elevation-1"
-                 :items-per-page="5">
-            <template v-slot:item.actions="{}">
-               <v-btn color="light-blue" x-small @click="{}">
-                  <v-icon light>
-                     mdi-file-download-outline
-                  </v-icon>
-               </v-btn>
-            </template>
+         <v-card-text>
+            <v-data-table
+                    v-model="selected"
+                    item-key="ID"
+                    dense
+                    :headers="columnNames"
+                    :items="paidInvoices"
+                    class="elevation-1"
+                    :items-per-page="5">
+               <template v-slot:item.actions="{}">
+                  <v-btn color="light-blue" x-small @click="{}">
+                     <v-icon light>
+                        mdi-file-download-outline
+                     </v-icon>
+                  </v-btn>
+               </template>
             </v-data-table>
+         </v-card-text>
          </v-card>
       </v-container>
    </v-main>
@@ -75,6 +217,10 @@
       components: {GenerateInvoice},
       data() {
          return {
+            startingDate: new Date().toISOString().substr(0, 10),
+            endDate: new Date().toISOString().substr(0, 10),
+            menuStartingDate: false,
+            menuEndDate: false,
             currentItem: {},
             showModal: false,
             selected: [],
@@ -207,6 +353,42 @@
             })
             computedColumnnames.push({text: 'Actions', value: 'actions', sortable: false })
             return computedColumnnames
+         },
+         getUniqueProperties(){
+            var array = [];
+            this.upcomingInvoices.forEach(function (item) {
+               if (!array.includes(item.Liegenschaft)){
+                  array.push(item.Liegenschaft)
+               }
+            })
+            return array
+         },
+         getUniqueAdministration(){
+            var array = [];
+            this.upcomingInvoices.forEach(function (item) {
+               if (!array.includes(item.Immobilienverwaltung)){
+                  array.push(item.Immobilienverwaltung)
+               }
+            })
+            return array
+         },
+         getUniqueTenants(){
+            var array = [];
+            this.upcomingInvoices.forEach(function (item) {
+               if (!array.includes(item.Mieter)){
+                  array.push(item.Mieter)
+               }
+            })
+            return array
+         },
+         getUniqueInvoiceCategory(){
+            var array = [];
+            this.upcomingInvoices.forEach(function (item) {
+               if (!array.includes(item.Rechnungsart)){
+                  array.push(item.Rechnungsart)
+               }
+            })
+            return array
          }
       }
    }
