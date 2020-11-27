faaa0acbb2287eec15f23a0796a50904bf7ef489
Keller Samuel (s) <samuel.keller@students.fhnw.ch>
2020-11-27 17:24:45 +0100

    make invoice button small

--- src/components/Invoices.vue
+++ src/components/Invoices.vue
@@ -14,7 +14,7 @@
               class="elevation-1"
               :items-per-page="15">
             <template v-slot:item.actions="{item}">
-              <v-btn @click="toCSV(item)">
+              <v-btn small @click="toCSV(item)">
                 <v-icon>mdi-file-download</v-icon>
               </v-btn>
             </template>
