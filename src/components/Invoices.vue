93730aafd9fdedf694f1f4ada8424db6e4b96170
Keller Samuel (s) <samuel.keller@students.fhnw.ch>
2020-11-27 15:00:10 +0100

    add new table in vcard

--- src/components/Invoices.vue
+++ src/components/Invoices.vue
@@ -39,44 +39,30 @@
 <!--</template>-->
 
 <template>
-  <div class="container-lg">
-    <h1 id = invoices>Rechnungen</h1>
-    <div class = dataTable>
-      <v-data-table
-          :headers="columnNames"
-          :items="Invoices"
-          class="elevation-1">
-        <template v-slot:item.actions="{item}">
-          <v-btn @click="toCSV(item)">
-            <v-icon>mdi-file-download</v-icon>
-          </v-btn>
-        </template>
-      </v-data-table>
-      <v-card max-width="300">
-        <v-list dense>
-          <v-subheader>Rechnungsvorlagen</v-subheader>
-          <v-list-item-group
-              v-model="selectedItem"
-              color="primary"
-          >
-            <v-list-item
-                v-for="(vorlagen, i) in vorlagen"
-                :key="i"
-            >
-              <v-list-item-content>
-                <v-list-item-title v-text="vorlagen.text"></v-list-item-title>
-              </v-list-item-content>
-              <v-list-item-icon>
-                <v-icon v-text="vorlagen.icon"></v-icon>
-              </v-list-item-icon>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
+  <v-main>
+    <v-container fluid>
+      <v-card style="margin-top: 20px" :elevation="5">
+        <v-card-title>
+          <h1>Anstehende Rechnungen</h1>
+          <v-badge :content="this.allInvoices.length" :value="this.allInvoices.length" color="success" inline/>
+        </v-card-title>
+        <v-card-text>
+          <v-data-table
+              dense
+              :headers="columnNames"
+              :items="allInvoices"
+              class="elevation-1"
+              :items-per-page="5">
+            <template v-slot:item.actions="{item}">
+              <GenerateInvoice :invoice="item"></GenerateInvoice>
+            </template>
+          </v-data-table>
+        </v-card-text>
       </v-card>
-    </div>
-  </div>
-
+    </v-container>
+  </v-main>
 </template>
+
 
 <script>
 
