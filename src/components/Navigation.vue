6d8f999c53f4b702d601148e484274e4d103ebb9
René Achterberg <rene.achterberg@bluewin.ch>
2020-12-01 10:26:53 +0100

    delete outcommented code

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -22,31 +22,6 @@
     </v-tabs>
   </v-card>
 </template>
-<!--<template>-->
-<!--  <div>-->
-<!--    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">-->
-<!--      <div class="collapse navbar-collapse" id="navbarNavDropdown">-->
-<!--        <ul class="navbar-nav">-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/dashboard" class="nav-link">Dashboard</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/invoices" class="nav-link">Rechnungen</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/loads" class="nav-link">Loads</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/facilities" class="nav-link">Anlagen</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/users" class="nav-link">Nutzer</router-link>-->
-<!--          </li>-->
-<!--        </ul>-->
-<!--      </div>-->
-<!--    </nav>-->
-<!--  </div>-->
-<!--</template>-->
 
 <script>
 export default {
