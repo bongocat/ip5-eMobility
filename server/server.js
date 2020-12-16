02d9fbe1c70447812de3b171af6075894ed8ee05
Keller Samuel (s) <samuel.keller@students.fhnw.ch>
2020-12-16 14:05:41 +0100

    add apirouter

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const apiRouter = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use('/api/megalog', apiRouter);
 
 app.listen(process.env.PORT || '3000', () => {
         console.log(`Server is running on port:  ${process.env.PORT || '3000 '}`)
