10b7f158f4894a9fe1731b8ce90f2cbf2839acc8
Keller Samuel (s) <samuel.keller@students.fhnw.ch>
2020-12-24 11:50:00 +0100

    formatting

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,29 +2,10 @@ const express = require('express');
 const db = require('../db');
 const router = express.Router();
 
-router.get('/usertype', async (req, res,next) => {
 
-    try {
-        let results = await db.usertype();
-        res.json(results);
-    } catch (e){
-        console.log(e);
-        res.sendStatus(500);
-    }
-
-});
-
-router.get('/usertype/:id', async (req, res,next) => {
-
-    try {
-        let results = await db.usertypeByID(req.params.id);
-        res.json(results);
-    } catch (e){
-        console.log(e);
-        res.sendStatus(500);
-    }
-
-});
+/**
+ * User
+ */
 
 router.get('/user', async (req, res,next) => {
 
@@ -42,6 +23,34 @@ router.get('/user/:id', async (req, res,next) => {
 
     try {
         let results = await db.userByID(req.params.id);
+        res.json(results);
+    } catch (e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+
+});
+
+/**
+ * Usertype
+ */
+
+router.get('/usertype', async (req, res,next) => {
+
+    try {
+        let results = await db.usertype();
+        res.json(results);
+    } catch (e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+
+});
+
+router.get('/usertype/:id', async (req, res,next) => {
+
+    try {
+        let results = await db.usertypeByID(req.params.id);
         res.json(results);
     } catch (e){
         console.log(e);
